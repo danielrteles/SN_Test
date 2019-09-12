5d5ffa4af41a0c330b02fc291e80aefd25dd25c5
Daniel Teles <danielrteles@gmail.com>
2019-09-12 16:54:47 +0100

    Added first Staging tables and SSIS project

---
+++ DB_Staging/staging/Tables/MetOffice.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [staging].[MetOffice] (
+    [CityName]  VARCHAR (20)   NULL,
+    [Year_yyyy] SMALLINT       NULL,
+    [Month_mm]  TINYINT        NULL,
+    [TMaxDegC]  DECIMAL (4, 1) NULL,
+    [TMinDegC]  DECIMAL (4, 1) NULL,
+    [AFDays]    TINYINT        NULL,
+    [RainMm]    DECIMAL (4, 1) NULL,
+    [SunHours]  DECIMAL (4, 1) NULL
+);
+
