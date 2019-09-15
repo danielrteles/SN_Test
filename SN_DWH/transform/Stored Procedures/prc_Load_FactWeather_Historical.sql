2b69ca96295e7b3ba0ed9b30ac5811a9ab7e4015
Daniel Teles <danielrteles@gmail.com>
2019-09-15 10:37:35 +0100

    Load of Historical Fact table

---
+++ SN_DWH/transform/Stored Procedures/prc_Load_FactWeather_Historical.sql
@@ -0,0 +1,34 @@
+﻿CREATE PROCEDURE transform.prc_Load_FactWeather_Historical
+AS
+BEGIN
+
+    INSERT INTO olap.FactWeather_Historical
+    SELECT  dg.GeographyKey
+    ,       dd.DateKey
+    ,       t.MinTemperature
+    ,       t.MaxTemperature
+    ,       t.AirFrostDays
+    ,       t.RainMm
+    ,       t.SunHours
+    FROM    transform.FactWeather_Historical t
+    JOIN    olap.DimDate dd         ON t.FullDate = dd.FullDate
+    JOIN    olap.DimGeography dg    ON dg.CityName = t.CityName
+                                    AND dg.CountryName = t.CountryName
+    WHERE   t.RecordAction = 'I'
+
+
+    UPDATE fwh
+    SET     MinTemperature  = t.MinTemperature
+    ,       MaxTemperature  = t.MaxTemperature
+    ,       AirFrostDays    = t.AirFrostDays
+    ,       RainMm          = t.RainMm
+    ,       SunHours        = t.SunHours
+    FROM    transform.FactWeather_Historical t
+    JOIN    olap.DimDate dd                 ON t.FullDate = dd.FullDate
+    JOIN    olap.DimGeography dg            ON dg.CityName = t.CityName
+                                            AND dg.CountryName = t.CountryName
+    JOIN    olap.FactWeather_Historical fwh ON fwh.GeographyKey = dg.GeographyKey
+                                            AND fwh.DateKey = dd.DateKey
+    WHERE   t.RecordAction = 'U'
+
+END
---
+++ SN_DWH/transform/Tables/FactWeather_Historical.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [transform].[FactWeather_Historical] (
+    [RecordAction]   CHAR (1)       NOT NULL,
+    [CityName]       VARCHAR (50)   NOT NULL,
+    [CountryName]    VARCHAR (50)   NOT NULL,
+    [FullDate]       DATE           NOT NULL,
+    [MinTemperature] DECIMAL (4, 1) NULL,
+    [MaxTemperature] DECIMAL (4, 1) NULL,
+    [AirFrostDays]   TINYINT        NULL,
+    [RainMm]         DECIMAL (4, 1) NULL,
+    [SunHours]       DECIMAL (4, 1) NULL
+);
+
