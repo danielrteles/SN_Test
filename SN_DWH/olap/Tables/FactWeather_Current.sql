a3bda853c9a500ac6dc2abbdde7abad5ec507b10
Daniel Teles <danielrteles@gmail.com>
2019-09-15 11:32:37 +0100

    Added DWH SQL Objects for Current weather

---
+++ SN_DWH/olap/Tables/FactWeather_Current.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [olap].[FactWeather_Current] (
+    [GeographyKey]   INT            NOT NULL,
+    [DateKey]        INT            NOT NULL,
+    [TimeKey]        INT            NOT NULL,
+    [MinTemperature] DECIMAL (5, 2) NULL,
+    [MaxTemperature] DECIMAL (5, 2) NULL,
+    [Humidity]       DECIMAL (5, 2) NULL,
+    CONSTRAINT [FK_FactWeather_Current_Date] FOREIGN KEY ([DateKey]) REFERENCES [olap].[DimDate] ([DateKey]),
+    CONSTRAINT [FK_FactWeather_Current_Geography] FOREIGN KEY ([GeographyKey]) REFERENCES [olap].[DimGeography] ([GeographyKey]),
+    CONSTRAINT [FK_FactWeather_Current_Time] FOREIGN KEY ([TimeKey]) REFERENCES [olap].[DimTime] ([TimeKey])
+);
+
---
+++ SN_DWH/transform/Stored Procedures/prc_Load_FactWeather_Current.sql
@@ -0,0 +1,34 @@
+﻿CREATE   PROCEDURE transform.prc_Load_FactWeather_Current
+AS
+BEGIN
+
+    INSERT INTO olap.FactWeather_Current
+    SELECT  dg.GeographyKey
+    ,       dd.DateKey
+    ,       dt.TimeKey
+    ,       t.MinTemperature
+    ,       t.MaxTemperature
+    ,       t.Humidity
+    FROM    transform.FactWeather_Current t
+    JOIN    olap.DimDate dd         ON dd.FullDate = CONVERT(DATE, t.FullDateTime)
+    JOIN    olap.DimTime dt         ON dt.StartTime = CONVERT(TIME, t.FullDateTime)
+    JOIN    olap.DimGeography dg    ON dg.CityName = t.CityName
+                                    AND dg.CountryName = t.CountryName
+    WHERE   t.RecordAction = 'I'
+
+
+    UPDATE  fwf
+    SET     MinTemperature  = t.MinTemperature
+    ,       MaxTemperature  = t.MaxTemperature
+    ,       Humidity        = t.Humidity
+    FROM    transform.FactWeather_Current t
+    JOIN    olap.DimDate dd                 ON dd.FullDate = CONVERT(DATE, t.FullDateTime)
+    JOIN    olap.DimTime dt                 ON dt.StartTime = CONVERT(TIME, t.FullDateTime)
+    JOIN    olap.DimGeography dg            ON dg.CityName = t.CityName
+                                            AND dg.CountryName = t.CountryName
+    JOIN    olap.FactWeather_Current fwf   ON fwf.GeographyKey = dg.GeographyKey
+                                            AND fwf.DateKey = dd.DateKey
+                                            AND fwf.TimeKey = dt.TimeKey
+    WHERE   t.RecordAction = 'U'
+
+END
