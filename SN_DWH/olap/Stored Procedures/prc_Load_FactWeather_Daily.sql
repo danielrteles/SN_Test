5c1e9d2d12cb129c84c7a3583bfa4fe3774a6be1
Daniel Teles <danielrteles@gmail.com>
2019-09-15 12:34:38 +0100

    Daily load and removed unnecessary Json references

---
+++ SN_DWH/olap/Stored Procedures/prc_Load_FactWeather_Daily.sql
@@ -0,0 +1,47 @@
+﻿CREATE   PROCEDURE olap.prc_Load_FactWeather_Daily
+AS
+BEGIN
+
+    DECLARE @CutOffDateKey INT
+    SELECT  @CutOffDateKey = MAX(DateKey)
+    FROM    olap.DimDate
+    WHERE   FullDate <= DATEADD(MONTH, -1, CONVERT(DATE, GETDATE()))
+
+    DELETE  
+    FROM    olap.FactWeather_Daily
+    WHERE   DateKey < @CutOffDateKey
+
+    DROP TABLE IF EXISTS #TempDailyWeather
+    SELECT  GeographyKey
+    ,       DateKey
+    ,       MIN(MinTemperature)                     AS MinTemperature
+    ,       MAX(MaxTemperature)                     AS MaxTemperature
+    ,       CONVERT(DECIMAL(5, 2), AVG(Humidity))   AS Humidity
+    INTO    #TempDailyWeather
+    FROM    olap.FactWeather_Current
+    GROUP BY GeographyKey
+    ,       DateKey
+    
+    INSERT INTO olap.FactWeather_Daily (GeographyKey, DateKey, MinTemperature, MaxTemperature, Humidity)
+    SELECT      t.GeographyKey
+    ,           t.DateKey
+    ,           t.MinTemperature
+    ,           t.MaxTemperature
+    ,           t.Humidity
+    FROM        #TempDailyWeather t
+    LEFT JOIN   olap.FactWeather_Daily fwd  ON t.GeographyKey = fwd.GeographyKey
+                                            AND t.DateKey = fwd.DateKey
+    WHERE       fwd.GeographyKey IS NULL
+
+    UPDATE  fwd
+    SET     MinTemperature  = t.MinTemperature
+    ,       MaxTemperature  = t.MaxTemperature
+    ,       Humidity        = t.Humidity
+    FROM    #TempDailyWeather t
+    JOIN    olap.FactWeather_Daily fwd  ON t.GeographyKey = fwd.GeographyKey
+                                        AND t.DateKey = fwd.DateKey
+    WHERE   fwd.MinTemperature  <> t.MinTemperature
+    OR      fwd.MaxTemperature  <> t.MaxTemperature
+    OR      fwd.Humidity        <> t.Humidity
+
+END
---
+++ SN_DWH/olap/Tables/FactWeather_Daily.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [olap].[FactWeather_Daily] (
+    [GeographyKey]   INT            NOT NULL,
+    [DateKey]        INT            NOT NULL,
+    [MinTemperature] DECIMAL (5, 2) NULL,
+    [MaxTemperature] DECIMAL (5, 2) NULL,
+    [Humidity]       DECIMAL (5, 2) NULL,
+    CONSTRAINT [PK_FactWeather_Daily] PRIMARY KEY CLUSTERED ([GeographyKey] ASC, [DateKey] ASC),
+    CONSTRAINT [FK_FactWeather_Daily_Date] FOREIGN KEY ([DateKey]) REFERENCES [olap].[DimDate] ([DateKey]),
+    CONSTRAINT [FK_FactWeather_Daily_Geography] FOREIGN KEY ([GeographyKey]) REFERENCES [olap].[DimGeography] ([GeographyKey]),
+);
+
+
+
